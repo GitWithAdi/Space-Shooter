cc9582d6806c53af327c0ae536a954afe981118b
GitWithAdi <adityapratap.1226@gmail.com>
2025-02-15 12:38:11 +0530

    Unity Game

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+/// <summary>
+/// The InputManager class centralizes all input handling for the player.
+/// It collects input data from the user and relays it to the PlayerController for movement and shooting.
+/// </summary>
+public class InputManager : MonoBehaviour
+{
+    public PlayerController playerController;
+
+    void Update()
+    {
+        // Forward movement input to the player controller
+        playerController.HandleMovement(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        // Forward shooting input
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerController.HandleShooting();
+        }
+    }
+}
